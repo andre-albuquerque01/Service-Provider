5e4983757b5e3caf09fb832fb820e68d0178fe14
andre <andrealbuquerque2001@gmail.com>
2024-05-17 17:05:53 -0300

    feat: update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { About } from '@/components/about'
 
 export default function Home() {
   return (
-    <main className="max-w-[1200px]  mx-auto">
+    <main className="">
       <About />
     </main>
   )
