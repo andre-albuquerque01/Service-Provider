b023e66003a096620d574e95f1c3e01e542dd8de
andre <andrealbuquerque2001@gmail.com>
2024-05-17 17:05:41 -0300

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { inter } from './font'
-import { Header } from '@/components/header/header'
+import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={` ${inter.className}`}>
         <Header />
         {children}
       </body>
