a9171d99433b3815449c93daa620569874e583c0
andre <andrealbuquerque2001@gmail.com>
2024-05-17 17:05:13 -0300

    feat: update font

--- src/app/font.ts
+++ src/app/font.ts
@@ -1,11 +1,15 @@
+/* eslint-disable camelcase */
 import { Inter, Roboto_Mono } from 'next/font/google'
- 
+
 export const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
 })
- 
-export const roboto_mono = Roboto_Mono({
+
+export const robotoMono = Roboto_Mono({
   subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
   display: 'swap',
+  variable: '--font-roboto',
+  style: ['normal', 'italic'],
 })
