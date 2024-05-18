c170e9003b4c88d1122fcb691fb19b5223fa216a
andre <andrealbuquerque2001@gmail.com>
2024-05-18 20:46:09 -0300

    feat: create new component

---
+++ src/components/differences.tsx
@@ -0,0 +1,33 @@
+import { Card } from './card'
+import { HeadLine } from './other/headLine'
+
+export const Differences = () => {
+  return (
+    <>
+      <div className="max-w-[1200px] mx-auto min-h-[99.7vh] flex flex-col justify-center gap-4 flex-wrap">
+        <h2 className="uppercase text-cyan-500 font-bold text-2xl text-center max-md:mt-20">
+          Diferenciais
+        </h2>
+        <div className="gradient-div w-[90%] h-0.5 mx-auto shadow-lg"></div>
+        <div className="flex items-center justify-center flex-wrap gap-3 max-md:p-3">
+          <Card
+            title="Soluções Personalizadas"
+            desc="Entendemos profundamente as necessidades específicas de cada cliente, oferecendo soluções personalizadas que superam expectativas."
+            url={'/personalizado.png'}
+          />
+          <Card
+            title="Inovação Constante"
+            desc="Exploramos continuamente novas tecnologias para fornecer soluções criativas e eficientes, mantendo nossos clientes na vanguarda do mercado."
+            url={'/inovacao.png'}
+          />
+          <Card
+            title="Compromisso com a Excelência"
+            desc="Aplicamos as melhores práticas da indústria e metodologias ágeis para garantir resultados excepcionais e soluções robustas."
+            url={'/acordo.png'}
+          />
+        </div>
+      </div>
+      <HeadLine />
+    </>
+  )
+}
