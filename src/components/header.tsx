9e1a08fd606d211c3bf343a5a3a561b2b53a32c8
andre <andrealbuquerque2001@gmail.com>
2024-05-18 20:47:34 -0300

    feat: update component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 
 export const Header = () => {
   const [navbar, setNavBar] = useState<boolean>(false)
+  const [isTop, setIsTop] = useState<boolean>(true)
 
   useEffect(() => {
     const desactivateNavBar = () => setNavBar(false)
@@ -11,11 +12,26 @@ export const Header = () => {
     return () => window.removeEventListener('resize', desactivateNavBar)
   }, [])
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY
+      if (scrollPosition > 0 && isTop) {
+        setIsTop(false)
+      } else if (scrollPosition === 0 && !isTop) {
+        setIsTop(true)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    // Cleanup function to remove event listener on component unmount
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [isTop])
+
   return (
     <header
-      className={`z-50 w-full fixed  transition-all ease-in-out duration-1000 bg-black   mb-[80px]`}
+      className={`z-50 w-full transition-all ease-in-out duration-1000 bg-black ${isTop ? 'block' : 'fixed'}`}
     >
-      <div className="mx-auto max-w-[1200px] bg-transparent flex justify-between py-4 items-center  px-4 text-gray-800 max-h-20 ">
+      <div className="mx-auto max-w-[1200px] bg-transparent flex justify-between py-4 items-center px-4 text-gray-800 max-h-20">
         <h2 className="text-white">Logo `-`</h2>
         <nav className="flex">
           <button
@@ -23,38 +39,38 @@ export const Header = () => {
             onClick={() => setNavBar((e) => !e)}
           >
             <hr
-              className={`bg-yellow-600 h-1  w-full rounded border-none transition-all transform absolute ${
-                navbar ? 'rotate-[50deg] top-2 ' : 'top-0 '
+              className={`bg-cyan-600 h-1 w-full rounded border-none transition-all transform absolute ${
+                navbar ? 'rotate-[50deg] top-2' : 'top-0'
               }`}
             />
             <hr
-              className={` h-1  w-full rounded border-none transition-all transform absolute  ${
-                navbar ? 'bg-transparent top-1 ' : 'bg-yellow-600 top-2'
+              className={`h-1 w-full rounded border-none transition-all transform absolute ${
+                navbar ? 'bg-transparent top-1' : 'bg-cyan-600 top-2'
               }`}
             />
             <hr
-              className={`bg-yellow-600 h-1  w-full rounded border-none transition-all transform absolute  ${
+              className={`bg-cyan-600 h-1 w-full rounded border-none transition-all transform absolute  ${
                 navbar ? 'rotate-[-50deg] top-2 ' : 'top-4'
               }`}
             />
           </button>
           <div
-            className={`  justify-centertransition-all z-50  transition-all w-full   flex-col  sm:w-auto  sm:flex-row sm:visible sm:flex  sm:gap-1 text-white  font-[700] items-center flex  duration-1000 overflow-hidden top-[80px] right-0 left-0 justify-start gap-9 fixed h-0 sm:h-auto sm:static ${
+            className={`justify-centertransition-all z-50 transition-all w-full flex-col sm:w-auto sm:flex-row sm:visible sm:flex sm:gap-1 text-white font-[700] items-center flex duration-1000 overflow-hidden top-[56px] right-0 left-0 justify-start gap-9 fixed h-0 sm:h-auto sm:static ${
               navbar
-                ? ' w-full t h-screen pt-12   bg-black'
+                ? 'w-full t h-screen pt-12 bg-black'
                 : 'invisible flex transition-all duration-1000'
             }`}
           >
             <Link
               href={`#inicio`}
-              className="transition duration-500  hover:bg-yellow-600 rounded-md px-4 py-3  cursor-pointer"
+              className="transition duration-500  hover:bg-cyan-600 rounded-md px-4 py-3  cursor-pointer"
               onClick={() => setNavBar(false)}
             >
               Inicio
             </Link>
             <Link
               href={`#about`}
-              className=" hover:bg-yellow-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer"
+              className=" hover:bg-cyan-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer"
               onClick={() => setNavBar(false)}
             >
               Quem Somos
@@ -62,21 +78,21 @@ export const Header = () => {
 
             <Link
               href={`#vendaaa`}
-              className=" hover:bg-yellow-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer"
+              className=" hover:bg-cyan-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer"
               onClick={() => setNavBar(false)}
             >
               Venda
             </Link>
             <Link
               href={`#servicos`}
-              className=" hover:bg-yellow-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer "
+              className=" hover:bg-cyan-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer "
               onClick={() => setNavBar(false)}
             >
               Serviços
             </Link>
             <Link
               href={`#ondeficamos`}
-              className=" hover:bg-yellow-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer"
+              className=" hover:bg-cyan-600 rounded-md transition duration-500 px-4 py-3 cursor-pointer"
               onClick={() => setNavBar(false)}
             >
               Onde nos Encontrar
