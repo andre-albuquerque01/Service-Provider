cb6b8dd6751e4dff6700ab8d482c4a8fc37f510c
andre <andrealbuquerque2001@gmail.com>
2024-05-22 20:42:50 -0300

    feat: update page

--- src/components/other/headLine.tsx
+++ src/components/other/headLine.tsx
@@ -1,3 +1,5 @@
 export const HeadLine = () => {
-  return <div className="w-[90%] mx-auto h-0.5 gradient-div-headline"></div>
+  return <div className="w-[90%] mx-auto h-0.5 gradient-div-headline">
+    
+  </div>
 }
