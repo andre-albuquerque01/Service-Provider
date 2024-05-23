e35139908783e819e2414f2151c263b089f2673b
andre <andrealbuquerque2001@gmail.com>
2024-05-23 16:15:47 -0300

    feat: update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { About } from '@/components/about'
 import { Differences } from '@/components/differences'
+import { Service } from '@/components/service'
 
 export default function Home() {
   return (
     <main className="">
       <About />
       <Differences />
+      <Service />
     </main>
   )
 }
