ea37f025fa008e78b11cc69888fb49786cd68d0e
andre <andrealbuquerque2001@gmail.com>
2024-05-23 16:46:06 -0300

    feat: create new component

---
+++ src/components/location.tsx
@@ -0,0 +1,12 @@
+export const Location = () => {
+  return (
+    <>
+      <div className="max-w-[1200px] mx-auto flex flex-col justify-center gap-4 flex-wrap">
+        <h2 className="uppercase text-cyan-500 font-bold text-2xl text-center max-md:mt-20">
+          Como nos localizar
+        </h2>
+        <div className="gradient-div w-[90%] h-0.5 mx-auto shadow-lg"></div>
+      </div>
+    </>
+  )
+}
