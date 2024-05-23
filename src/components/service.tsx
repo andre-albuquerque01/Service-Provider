a48e2cc3f3a5124d3251989fce65da64ec6ef373
andre <andrealbuquerque2001@gmail.com>
2024-05-23 16:16:19 -0300

    feat: create new component

---
+++ src/components/service.tsx
@@ -0,0 +1,68 @@
+export const Service = () => {
+  return (
+    <>
+      <div className="max-w-[1200px] mx-auto min-h-[99.7vh] flex flex-col justify-center gap-4 flex-wrap">
+        <h2 className="uppercase text-cyan-500 font-bold text-2xl text-center max-md:mt-20">
+          O que fazemos
+        </h2>
+        <div className="p-4 grid grid-cols-2 gap-8 text-center max-sm:grid-cols-1">
+          <div>
+            <p className="font-bold">Desenvolvimento de Software:</p> Criamos
+            soluções personalizadas para atender às necessidades específicas do
+            seu negócio.
+          </div>
+          <div className="">
+            <p className="font-bold">Hospedagem:</p> Oferecemos serviços de
+            hospedagem confiáveis para garantir que seus sistemas estejam sempre
+            acessíveis.
+          </div>
+          <div>
+            <p className="font-bold">Sistemas Completos:</p> Desenvolvemos
+            sistemas integrados e robustos que cobrem todas as funcionalidades
+            necessárias.
+          </div>
+          <div>
+            <p className="font-bold">Layout Intuitivo:</p> Priorizamos a criação
+            de interfaces fáceis de usar, proporcionando uma excelente
+            experiência para o usuário.
+          </div>
+          <div>
+            <p className="font-bold">Consultoria Técnica:</p> Fornecemos
+            orientação especializada para ajudar a definir a melhor abordagem
+            tecnológica para seus projetos.
+          </div>
+          <div>
+            <p className="font-bold">Manutenção e Suporte:</p> Oferecemos
+            serviços contínuos de manutenção e suporte para garantir que seus
+            sistemas funcionem sem problemas.
+          </div>
+          <div>
+            <p className="font-bold">Integração de Sistemas:</p> Facilitamos a
+            integração de novos sistemas com suas soluções existentes,
+            garantindo uma operação harmoniosa.
+          </div>
+          <div>
+            <p className="font-bold">Segurança:</p> Implementamos as melhores
+            práticas de segurança para proteger seus dados e garantir a
+            integridade dos sistemas.
+          </div>
+          <div>
+            <p className="font-bold">Escalabilidade:</p> Desenvolvemos soluções
+            que crescem junto com o seu negócio, permitindo fácil expansão e
+            adaptação a novas demandas.
+          </div>
+          <div>
+            <p className="font-bold">Metodologias Ágeis:</p> Utilizamos
+            metodologias ágeis para garantir entregas rápidas e eficientes, com
+            foco na qualidade e satisfação do cliente.
+          </div>
+          <div>
+            <p className="font-bold">Inovação Contínua:</p> Estamos sempre
+            atualizados com as últimas tecnologias e tendências do mercado para
+            oferecer soluções inovadoras.
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
