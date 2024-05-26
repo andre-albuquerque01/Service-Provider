79475e1d49be6cc5ad3749e575ce32901d822ea3
andre <andrealbuquerque2001@gmail.com>
2024-05-26 13:20:10 -0300

    feat: create new component

---
+++ src/components/projects.tsx
@@ -0,0 +1,33 @@
+import { CardProject } from './other/cardProject'
+import { HeadLine } from './other/headLine'
+
+export const Projects = () => {
+  return (
+    <>
+      <div className="max-w-[1200px] mx-auto min-h-[99.7vh] flex flex-col justify-center gap-4 flex-wrap">
+        <h2 className="uppercase text-cyan-500 font-bold text-2xl text-center max-md:mt-20">
+          Projetos desenvolvidos
+        </h2>
+        <div className="gradient-div w-[90%] h-0.5 mx-auto shadow-lg"></div>
+        <div className="flex items-center justify-center flex-wrap gap-3 max-md:p-3">
+          <CardProject
+            title="Sistema de cardápio digital"
+            desc="O Cardápio Online é uma plataforma que permite aos clientes acompanhar facilmente os pedidos realizados através do garçom, verificar os itens selecionados, o valor total da conta e facilita o cadastro através de um identificador exclusivo para localizar os pedidos."
+            url={'/project/menu.png'}
+          />
+          <CardProject
+            title="Rede social"
+            desc="Rede social estilo fórum, projetada para promover a interação sobre os tópicos no feed, permitindo que os usuários se comuniquem entre si."
+            url={'/differences/inovacao.png'}
+          />
+          <CardProject
+            title="Portifolio pessoal"
+            desc="Portfólio para um profissional de TI que apresenta informações sobre ele, aumentando sua visibilidade com uma marca própria na sua landing page."
+            url={'/differences/acordo.png'}
+          />
+        </div>
+      </div>
+      <HeadLine />
+    </>
+  )
+}
